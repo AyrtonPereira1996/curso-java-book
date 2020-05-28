cf4feeba507214106080b0222a6a065541a327f0
AyrtonPereira1996 <ayrton_pereira1996@hotmail.com>
2020-05-28 10:27:04 +0200

    add section about some examples

---
+++ Cap6-examples/src/example/MathJavaLangLibrary.java
@@ -0,0 +1,9 @@
+package example;
+
+public class MathJavaLangLibrary {
+	public static void main(String[] args) {
+		double x = 3.14;
+		double f = java.lang.Math.log(x);
+		System.out.println(f);
+	}
+}
---
+++ Cap6-examples/src/example/StringJavaLangLibrary.java
@@ -0,0 +1,12 @@
+package example;
+
+public class StringJavaLangLibrary {
+
+	public static void main(String[] args) {
+		String texto = "Este texto serve apenas para teste";
+		int x = texto.indexOf('e');
+		System.out.println(x);
+
+	}
+
+}
